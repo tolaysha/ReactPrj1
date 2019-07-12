5f9db72edffb8f20fcc68a2ce486c8b664622f1e
Anatoly Kuzmin <tolaykuzay@gmail.com>
2019-07-12 17:02:12 +0300

    first steps

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	return <p>Всем привет, я компонент App</p>
+}
+const News = () => {
+	return <p>К сожалению, новостей нет</p>
+  }
+class BigApp extends React.Component {
+	render() {
+	  return (
+		<React.Fragment>
+		  <h1>Я компонент, BigApp</h1>
+		  <p className='red'>Компоненты можно вкладывать друг в друга.</p>
+		  <App/>
+		  <News/>
+		</React.Fragment>
+	  )
+	}
 }
 
-export default App;
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React pls
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+//export default App;
+export default BigApp;
